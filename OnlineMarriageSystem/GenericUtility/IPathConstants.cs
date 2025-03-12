14b34a88923ac82e8e0492f12873e86265565b02
.𝓻𝓪𝓷𝓳𝓪𝓷𝓪𝓭𝓪𝓻𝓼𝓱 . <ranjanadarsh1996@outlook.com>
2025-03-12 15:47:02 +0530

    Add project files.

---
+++ OnlineMarriageSystem/GenericUtility/IPathConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1_nunit.Framework.Framework_GenericUtility
+{
+    internal interface IPathConstants
+    {
+        public static string ExcelPath = "C:\\Users\\ranja\\source\repos\\OnlineMarriageSystem\\OnlineMarriageSystem\\TestData\\matrimony.xlsx";
+        public static string jsonpath = "C:\\Users\ranja\\source\\repos\\OnlineMarriageSystem\\OnlineMarriageSystem\\TestData\\CommonData.json";
+
+    }
+}
---
+++ OnlineMarriageSystem/POM_repo/SigninPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarriageSystem.POM_repo
+{
+    class SigninPage
+    {
+        SigninPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+        [FindsBy(How = How.XPath, Using="//input[@type='text']")]
+        private IWebElement usernametextfield;
+
+        [FindsBy(How = How.XPath, Using="//input[@type='password']")]
+        private IWebElement passwordtextfield;
+
+        [FindsBy(How = How.XPath, Using="//button[@type='submit']")]
+        private IWebElement signinbutton;
+
+        public IWebElement Usernametextfield { get => usernametextfield; }
+        public IWebElement Passwordtextfield { get => passwordtextfield;  }
+        public IWebElement Signinbutton { get => signinbutton; }
+    }
+}
