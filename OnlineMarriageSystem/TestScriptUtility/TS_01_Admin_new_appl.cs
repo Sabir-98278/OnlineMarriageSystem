5e4ac381e8a525bb3a6853188e4f6e4d7d43a308
.𝓻𝓪𝓷𝓳𝓪𝓷𝓪𝓭𝓪𝓻𝓼𝓱 . <ranjanadarsh1996@outlook.com>
2025-03-17 11:19:04 +0530

    aa

---
+++ OnlineMarriageSystem/TestScriptUtility/TS_01_Admin_new_appl.cs
@@ -0,0 +1,28 @@
+﻿using OnlineMarriageSystem.GenericUtility;
+using OnlineMarriageSystem.POM_repo;
+using OpenQA.Selenium;
+using TestProject1_nunit.Framework.Framework_GenericUtility;
+
+namespace OnlineMarriageSystem.TestScriptUtility
+{
+    class TS_01_Admin_new_appl :BaseforUser
+    {
+        [Test]
+        public void applnew()
+        {
+            WebDriverUtility webutil = new WebDriverUtility();
+            HomePage hp = new HomePage(driver);
+        //    driver.FindElement(By.XPath(""))
+            webutil.ExplicitwaitElementsClickable(driver,By.XPath("//a[contains(text(),'New')]"));
+            hp.Newapplicationlink.Click();
+            hp.Infoeyeicon.Click();
+            ViewMarriageAppPage view = new ViewMarriageAppPage(driver);
+            
+            webutil.ScrolltoViewUsingJavaScriptExecutor(driver, view.TakeActionbutton1);
+            view.TakeActionbutton1.Click();
+            view.Remarkstextfield.SendKeys("rejected");
+            webutil.ToHandleDropdown(view.Statusdropdown, "Rejected");
+            view.Closebutton.Click();
+        }
+    }
+}
